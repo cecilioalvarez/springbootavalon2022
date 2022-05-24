c8fe43d7b673f9dbc4fc61cb2d34b53e026ddf68
jcalat <jcalataruiz@hotmail.com>
2022-05-24 14:21:39 +0200

    RESTController JSON  de Persona añadimos borrar

--- src/main/java/es/avalon/web1/rest/PersonaRESTController.java
+++ src/main/java/es/avalon/web1/rest/PersonaRESTController.java
@@ -7,12 +7,18 @@ import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-
-@RestController
+//Este controlador es el que usamos para usar datos puros con la base de datos. Es decir a traves de JASON
+//Añadimos los metodos de listar, añadir, borrar....
+@RestController//Anotacion para definir como un controller de JSON(JavaScript Object Notation) "Controlador REST".
 @RequestMapping("/webapi/personas")
+@CrossOrigin(origins = "*", methods={RequestMethod.GET,RequestMethod.POST})
 public class PersonaRESTController {
 
     @Autowired
+    //Lo que hace un autowired es buscar un objeto manejado (beans) que
+    // implementen determinada interfaz para hacer referencia a él.
+    // DE esta manera no es neceario crear una instancia nueva del objeto
+    // cada vez que se necesite la funcionalidad de determinada clase.
     private LibroPersonaService servicio;
 
     @GetMapping
